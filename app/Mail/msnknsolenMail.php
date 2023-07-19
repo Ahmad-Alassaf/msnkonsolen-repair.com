cbea967215bafd1c55716893380c0ab3f451986e
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-19 10:45:39 +0200

    email with wellkommenemail9.blade.php

--- app/Mail/msnknsolenMail.php
+++ app/Mail/msnknsolenMail.php
@@ -37,7 +37,7 @@ class msnknsolenMail extends Mailable
             replyTo: [
                 new Address('info@msnkonsolen-repair.com', 'Ahmad Alassaf')
             ],
-            subject:$this->mailData->subject,
+            subject:$this->mailData['subject'],
            
         );
     }
