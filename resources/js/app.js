4b19a59b70526d311ecc421e618fc3408f551465
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-19 14:52:08 +0200

    sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,12 @@ import '../sass/app.scss';
 import { createApp } from 'vue';
 import Router from '@/router';
 import store from '@/store';
-import VueSimpleAlert from "vue-simple-alert";
+
 
 const app = createApp({});
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import home from '@/components/home.vue'
 app.component('home', home);
 
-app.use(Router).use(store).use(VueSimpleAlert).mount('#app');
+app.use(Router).use(store).use(VueSweetalert2).mount('#app');
--- resources/js/components/sendemail.vue
+++ resources/js/components/sendemail.vue
@@ -55,7 +55,7 @@ export default {
             await axios.post(`/api/sendemail`,fd,config)
             .then((response)=>{
                                 
-                               
+                               this.$swal('first alert')
                 
           
             })
