cb5f253cae6372bd450e054d252545483c6894c4
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-20 08:58:19 +0200

    sendemail responsive

--- resources/js/components/sendemail.vue
+++ resources/js/components/sendemail.vue
@@ -2,12 +2,12 @@
     <navbar />
     <jambotron />
     <div class="container py-5">
-        <form @submit.prevent="sendemail()" class="form rounded px-3 py-2 w-50 justify-content-center m-auto shadow">
+        <form @submit.prevent="sendemail()" class="form rounded px-3 py-2 w-lg-50  justify-content-center m-auto shadow">
             <h3 class="mb-1 bg-secondary py-1 px-2 rounded text-white"> E-Mail</h3>
             <input type="text" v-model="mailData.from" placeholder="Ihre Email Addresse..." class="form-control mb-1" name="to" id="">
             <input type="text" v-model="mailData.subject" placeholder="Subject..." class="form-control mb-1" name="subject" id="">
             <textarea name="content" v-model="mailData.content" placeholder="Message..." cols="30" class="form-control mb-2" rows="10"></textarea>
-            <input type="submit" value="Absenden " class="btn btn-primary mb-2 ">
+            <input type="submit" value="Absenden " class="btn btn-primary m-2  d-block m-auto">
 
         </form>
        
