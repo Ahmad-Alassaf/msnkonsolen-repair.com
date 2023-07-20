789de93e23c0766cc299d12e57252c7236b3494f
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-20 12:14:52 +0200

    contact us page 1

--- resources/js/components/contact.vue
+++ resources/js/components/contact.vue
@@ -3,19 +3,52 @@
        
             <navbar />
             <jambotron />
-            <h1>
-                Contact Page
-            </h1>
+            <div class="container">
+                <div class="row py-5">
+                    <div class=" row  col-md-4 bg-secondary py-3 rounded">
+                        <div class="col-12  py-1">
+                            <h3 class="text-white">Name</h3>
+                            <p class="text-white px-3">MSN Konsolen Reparatur</p>
+
+                        </div>
+                        <div class="col-12">
+                            <h3 class="text-white">Email</h3>
+                            <p class="text-white px-3">info@msnkonsolen-repair.com</p>
+
+                        </div>
+                        <div class="col-12">
+                            <h3 class="text-white">Phone</h3>
+                            <p class="text-white px-3">004915175614666</p>
+
+                        </div>
+                        <div class="col-12">
+                            <h3 class="text-white">Adresse</h3>
+                            <p class="text-white px-3">Nordweg. 1, 87730 Badgrönenbach, Deutschlan  </p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-md-8">
+                        <sendemail />
+
+                    </div>
+                </div>
+            </div>
+            
+
+                
+          
         
     </div>
 </template>
 <script>
 import navbar from "./layouts/navbar.vue";
 import jambotron from './layouts/jambotron.vue';
+import sendemail from './sendemail.vue'
 export default {
     name:"contact",
     components:{
-        
+        sendemail,
         navbar,      
         jambotron,    
     },
--- resources/js/components/sendemail.vue
+++ resources/js/components/sendemail.vue
@@ -1,28 +1,26 @@
 <template >
-    <navbar />
-    <jambotron />
-    <div class="container py-5">
+  
+    <div class=" ">
         <form @submit.prevent="sendemail()" class="form rounded px-3 py-2 w-lg-50  justify-content-center m-auto shadow">
-            <h3 class="mb-1 bg-secondary py-1 px-2 rounded text-white"> E-Mail</h3>
+            <h3 class="mb-1 bg-secondary py-1 px-2 rounded text-white"> Ihre Nachricht</h3>
             <input type="text" v-model="mailData.from" placeholder="Ihre Email Addresse..." class="form-control mb-1" name="to" id="">
-            <input type="text" v-model="mailData.subject" placeholder="Subject..." class="form-control mb-1" name="subject" id="">
-            <textarea name="content" v-model="mailData.content" placeholder="Message..." cols="30" class="form-control mb-2" rows="10"></textarea>
+            <input type="text" v-model="mailData.subject" placeholder="Betriff..." class="form-control mb-1" name="subject" id="">
+            <textarea name="content" v-model="mailData.content" placeholder="Ihre Nachricht..." cols="30" class="form-control mb-2" rows="10"></textarea>
             <input type="submit" value="Absenden " class="btn btn-primary m-2  d-block m-auto">
 
         </form>
        
         
     </div>
-    <foot />   
+   
 </template>
 <script>
-import navbar from "./layouts/navbar.vue";
-import jambotron from './layouts/jambotron.vue';
-import foot from './layouts/foot.vue';
+
+
 import { mapGetters,mapActions } from 'vuex';
 export default {
     name:"sendemail",
-    components:{navbar,jambotron,foot},
+    components:{},
     data(){
         return {
             mailData:{
