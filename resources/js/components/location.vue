825b6ff6200f9ee8cfd784a870c0e08707651c9d
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-21 06:27:31 +0200

    google map added 2

--- resources/js/components/location.vue
+++ resources/js/components/location.vue
@@ -5,66 +5,26 @@
     <GoogleMap
     :zoom="14"    
     :center="center"
-  api-key="AIzaSyCBcDY3KDhse6SeN6QvUf7RS3YY1cLXLS8"
+     api-key="AIzaSyCBcDY3KDhse6SeN6QvUf7RS3YY1cLXLS8"
   style="width: 100%; height: 300px"
-  >
+  >   <Marker :options="{ position: center }" />
   </GoogleMap>
   </div>
 </template>
 
 <script>
-
+import { defineComponent } from "vue";
 import { 
   GoogleMap, // core
   Marker,
-  Polyline,
-  Polygon,
-  Rectangle,
-  Circle,
-  CustomControl,
+ 
 } from 'vue3-google-map'
-export default {
-  name: "location",
-  data() {
-    return {
-      center: { 
-        lat: 39.7837304,
-        lng: -100.4458825
-      },
-      locationMarkers: [],
-      locPlaces: [],
-      existingPlace: null
-    };
-  },
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 };
 
-  mounted() {
-    this.locateGeoLocation();
+    return { center };
   },
-
-  methods: {
-    initMarker(loc) {
-      this.existingPlace = loc;
-    },
-    addLocationMarker() {
-      if (this.existingPlace) {
-        const marker = {
-          lat: this.existingPlace.geometry.location.lat(),
-          lng: this.existingPlace.geometry.location.lng()
-        };
-        this.locationMarkers.push({ position: marker });
-        this.locPlaces.push(this.existingPlace);
-        this.center = marker;
-        this.existingPlace = null;
-      }
-    },
-    locateGeoLocation: function() {
-      navigator.geolocation.getCurrentPosition(res => {
-        this.center = {
-          lat: res.coords.latitude,
-          lng: res.coords.longitude
-        };
-      });
-    }
-  }
-};
+});
 </script>
