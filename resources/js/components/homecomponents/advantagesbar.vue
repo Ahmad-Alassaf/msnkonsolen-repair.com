45c4f2c961d9b290ee294684f32a96c16cd1bc20
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-24 02:26:32 +0200

    advanteg bar responsive

--- resources/js/components/homecomponents/advantagesbar.vue
+++ resources/js/components/homecomponents/advantagesbar.vue
@@ -2,22 +2,22 @@
     <div class="bg-white advanteg-banner">
         <div class="container py-3  ">
             <div class="row align-items-center justify-content-between">
-                <div class="advanteg-banner-item col-lg-2  shadow  rounded col text-center  py-4 px-1 ">
+                <div class="advanteg-banner-item mb-2 col-xs-12 col-sm-12 col-md-5  col-lg-2  shadow  rounded col text-center  py-4 px-1 ">
                     <span class="fst-italic   "> Schnelle Reparatur</span>
                 </div>
                
-                <div class="advanteg-banner-item col-lg-2  shadow rounded col text-center py-4 px-1">
+                <div class="advanteg-banner-item mb-2 col-xs-12 col-sm-12 col-md-5 col-lg-3  shadow rounded col text-center py-4 px-1">
                     <span class="fst-italic   ">
                         Sichere und Leichte Zahlung
                     </span>
                   
                 </div>
-                <div class="advanteg-banner-item col-lg-2  shadow rounded col text-center py-4 px-1">
+                <div class="advanteg-banner-item mb-2 col-xs-12 col-sm-12 col-md-5 col-lg-2  shadow rounded col text-center py-4 px-1">
                     <span class="fst-italic   ">
                         Saubere Arbeit
                     </span>                   
                  </div>
-                 <div class="advanteg-banner-item col-lg-2  shadow rounded col text-center py-4 px-1">
+                 <div class="advanteg-banner-item mb-2 col-xs-12 col-sm-12 col-md-5 col-lg-2  shadow rounded col text-center py-4 px-1">
                     <span class="fst-italic   ">
                         6 Monate Waranty
                     </span>                   
