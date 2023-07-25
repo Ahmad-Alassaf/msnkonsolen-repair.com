1380c4bd683915661ac554083f5c1befd89afdd3
unknown <eng.ahmad.alassaf@gmail.com>
2023-07-25 05:50:04 +0200

    nintedo cards added

--- resources/js/components/homecomponents/nintendo.vue
+++ resources/js/components/homecomponents/nintendo.vue
@@ -1,21 +1,86 @@
 <template >
     <div class="container py-5">
-        <h1>Nintedo</h1>
-        <div class="row">
-            <div class="col-3 ">
-              <img src="../../../../public/storage/images/joycons.jpg" style=" border-radius:50%  " class="card-img-top " alt="...">
-             </div>
-             <div class="col-3">
-              <img src="../../../../public/storage/images/joycons.jpg" style=" border-radius:50% " class="card-img-top " alt="...">
-             </div>
-             <div class="col-3">
-              <img src="../../../../public/storage/images/joycons.jpg" style=" border-radius:50% " class="card-img-top " alt="...">
-             </div>
-             <div class="col-3">
-              <img src="../../../../public/storage/images/joycons.jpg" style=" border-radius:50% " class="card-img-top " alt="...">
-             </div>
-            
-  
+
+        <div class=" row  justify-content-md-around">
+            <div class="card  p-0 bg-none col-10 col-md-6 col-lg-4 mb-2">
+                <div class="card-header text-center">
+                    <h3>Nintendow Switch 32GB</h3>
+                </div>
+                <div>
+                    <img src="../../../../public/storage/images/nintendo/1.jpg" style="" class="card-img-top rounded-0" alt="...">
+
+                </div>
+                <div class="card-body">
+                   <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <span>Display, Touch </span>
+                    </li>
+                    <li class="list-group-item">
+                        <span>Spielslot </span>
+                    </li>
+                    <li class="list-group-item">
+                        <span>Micro SD-Karte </span>
+                    </li>
+                    <li class="list-group-item">
+                        <span>Akku </span>
+                    </li>
+                    <li class="list-group-item">
+                        <span>Luefter </span>
+                    </li>
+                   </ul>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary d-block m-auto ">und Mehr</button>
+                </div>
+            </div>
+             <div class="card p-0 col-10 col-md-6 col-lg-4 mb-2">
+                <div class="card-header text-center">
+                    <h3> Analogstick</h3>
+                </div>
+                <div>
+                    <img src="../../../../public/storage/images/nintendo/3.jpg" style="  " class="card-img-top rounded-0" alt="...">
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span>Analogstick drifft </span>
+                        </li>
+                        <li class="list-group-item">
+                            <span>Analogsick gebrochen </span>
+                        </li>
+                        <li class="list-group-item">
+                            <span>Kein Funcktion </span>
+                        </li>                        
+                       </ul>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary d-block m-auto ">und Mehr</button>
+                </div>              
+            </div>
+            <div class="card p-0 col-10 col-md-8 col-lg-4 mb-2">
+                <div class="card-header text-center">
+                    <h3> Joy-con Führungsschiene</h3>
+                </div>
+                <div>
+                    <img src="../../../../public/storage/images/nintendo/2.jpg" style="" class="card-img-top rounded-0" alt="...">
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span>Kein Verbindung </span>
+                        </li>
+                        <li class="list-group-item">
+                            <span>Akku Lädt nicht</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span>Hält nicht fest</span>
+                        </li>                        
+                       </ul>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary d-block m-auto ">und Mehr</button>
+                </div>              
+            </div> 
           </div>
         
     </div>
