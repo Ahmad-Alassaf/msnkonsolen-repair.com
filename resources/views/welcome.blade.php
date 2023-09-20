2fa57d51551e3fe13dd0b0f3f71c707b8d165034
unknown <eng.ahmad.alassaf@gmail.com>
2023-09-20 05:59:04 +0200

    error corrected

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        @vite(['resources/js/app.js','resources/css/app.css'])
+        @vite(['resources/js/app.js'])
     </head>
     <body>
         <div id="app">
