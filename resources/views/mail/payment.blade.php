e21a869580d56e95daba5e3157488966e516d4a7
unknown <eng.ahmad.alassaf@gmail.com>
2023-10-01 23:32:01 +0200

    email foeach

--- resources/views/mail/payment.blade.php
+++ resources/views/mail/payment.blade.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Zahlung Bestätigung</title>
+</head>
+<body>
+  
+<ul>
+@foreach ($contracts as $contract)
+    <li>{{$contract->jobsnubmer}}-{{$contract->device}} -{{$contract->serialnumber}}</li>
+@endforeach
+</ul>
+  
+</body>
+</html>
 <h1>
     customer has payed 
    
 </h1>
- contracts:{{$contracts}}
+ 
